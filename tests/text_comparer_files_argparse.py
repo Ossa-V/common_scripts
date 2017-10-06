bc1df07aa683fd4a0843918142f8428169e16dfa
Kajmer <8766366+Kajmer@users.noreply.github.com>
2017-10-06 16:18:30 +0500

    Close file

--- tests/text_comparer_files_argparse.py
+++ tests/text_comparer_files_argparse.py
@@ -1,57 +1,62 @@
 #!/usr/bin/python
-import sys
 import argparse
 
 '''
 The script deletes the data in the first file that are found in the second file.
 Is able to work with data in a column and in a string (must be separated by a space).
 Prints the data in a column
-
 To Do: Add work without outputfile (output into console via print() )
 '''
 
-#Scripts removes entries in file_to_correct
+# Scripts removes entries in inputfile which was found in correct file
 
-# Проблема:
-# Скрипт работает обязательно с файлами. И обязательно с тремя.
-# Соответственно задача - обеспечить возможность для работы с текстовыми данными, вне файла.
-# Обеспечить следующие варианты:
-# 1) Файл - фраза - вывод в консоль
-# 2) Файл - фраза - вывод в файл
-# 3) Файл - файл  - вывод в консоль
-# 4) Файл - файл  - вывод в файл
-# Первый элемент будет файл, т.к. предполагается, что в нем большой объем данных. 
-# Подумать как можно организовать передачу данных по трубе.
-# Добавить отключение отладочной информации в случае вывода в консоль. Либо перенесети её в верхнюю часть (всю).
+parser = argparse.ArgumentParser(description='Hey, i\'m description, don\'t touch me!')
+parser.add_argument("-i", "--input", type=str, help="Input file to correct")
+parser.add_argument("-c", "--correct", type=str, help="File with lines will be deleted in input file")
+parser.add_argument("-C", type=str, help="Text line for exclude words from input file") #dunno how it's gonna work with "c" arg and "C".
+parser.add_argument("-o", "--output", type=str, help="Write to file")
+parser.add_argument("-s", "--silent", help="Turn off statistic", default=False, action="store_true")
 
-def parse_args():
-	parser = argparse.ArgumentParser(description='Hey, i\'m description, don\'t touch me!')
-	parser.add_argument("-i", "--input", help="Input file to correct")
-	parser.add_argument("-c", "--correct", help="File with lines will be deleted in input file")
-	parser.add_argument("-o", "--output", help="Write to file")
+args = parser.parse_args()
 
+if (args.input == None):
+    print("[-]\tNo input file specified! Exiting..")
+    exit(1)
+else:
+    inputText = (open(args.input).read()).split() # Read input file and split text into list
 
-if len(sys.argv) < 4:
-	print("{} <file_to_correct> <comparing_file> <output_file>".format(sys.argv[0]))
-	print("The script deletes the data in the first file that are found in the second file.\n		
-		Is able to work with data in a column and in a string (must be separated by a space).\n
-		Prints the data in a column.")
-	sys.exit(1)
+    compareText = ""
+    if (args.correct != None): #Now we gonna try to read compare text or string
+        compareText = (open(args.correct).read().split()) # Read correct file and split text into list. # To Do: Add exception here, if we couldn't open file.
+    elif (args.C != None):
+        compareText = args.C
+    else:
+        print ("[-]\tCouldn't read correct file or string. Exiting...")
+        exit(1)
 
-inputFile = open(sys.argv[1])
-comparingFile = open(sys.argv[2])
-outputFile = open(sys.argv[3], 'w')
+    if (args.silent == False):
+        print("[*]\tIn input file {} objects".format(len(inputText)))
+        print("[*]\tIn compare file {} objects".format(len(compareText)))
 
-inputText = (inputFile.read()).split()
-compare = (comparingFile.read()).split()
-print("In input file {} objects".format(len(inputText)))
-print("In compare file {} objects".format(len(compare)))
-counter = 0
-for inputItem in inputText:
-	if inputItem not in compare:
-		outputFile.write(inputItem+"\n")
-		counter += 1
-	else:
-		pass
+    #Debug
+    #print ("Input Text:\n{}".format(inputText))
+    #print ("CompareText:\n{}".format(compareText))
 
-print("In output file {} objects".format(counter))
+    outputFile = open(args.output, 'w')
+    counter = 0
+    for inputItem in inputText:
+        if inputItem not in compareText:
+            if (args.output != None): #if item not in compare we write it to the output
+        #print("Output: {}".format(inputItem)) Debug
+                outputFile.write(inputItem + "\n") #Add Exception if we couldn't open file
+            else:
+                print(inputItem)
+        else:
+        #Debug
+        #print ("Input {} found in {}!".format(inputItem, compareText))
+            pass #if item in compare list we skip it
+
+    if (args.silent == False):
+        print("[*]\tIn output file {} objects".format(counter))
+
+    outputFile.close()
