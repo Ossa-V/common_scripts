996971e1e764730601c1e181b659aea128ecf494
Kajmer <8766366+Kajmer@users.noreply.github.com>
2017-10-20 13:21:07 +0500

    Update http_parser_title.py

--- http_parser_title.py
+++ http_parser_title.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*- 
-#we got some problems with ecndoing, need to solve (when we write to the file or using pipe we got error)
+# -*- coding: utf-8 -*-
+# Works only with file output
 import lxml.html as html
+import argparse
 
-# http://www.edudic.ru/fam/1/
-for item in range(1,15151):
+def progress(counter): # Status report
+	if counter % 5 == 0:
+		print ("{} objects done...\r".format(counter))
+
+parser = argparse.ArgumentParser(description="Parses title of pages (remove text[0:-60] for full title)")
+parser.add_argument("-o", "--output", type=str, help="Write to file (if empty - write to console")
+args = parser.parse_args()
+
+if args.output != None:
+	outputFile = open(args.output, 'w')
+else:
+	print("Output file not specified")
+
+# http://www.edudic.ru/fam/1/ # last 15181
+lastitem = 20
+for item in range(1,lastitem + 1):
 	page = html.parse('http://www.edudic.ru/fam/{}/'.format(item))
-	print (page.find(".//title").text[0:-60])
+	outputFile.write(page.find(".//title").text[0:-60].encode('utf-8')+"\r\n")
+	progress(item)
+
+print ("FIN")
+if args.output != None:
+	outputFile.close()
