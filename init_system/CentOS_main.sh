83fee44f57ac0f53d56bcc3c0ad117f1335b2999
Kajmer <8766366+Kajmer@users.noreply.github.com>
2017-11-06 09:37:15 +0500

    Update CentOS_main.sh

--- init_system/CentOS_main.sh
+++ init_system/CentOS_main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-yum -y update
+yum -y update # Обновить систему, при этом сохранить устаревшие версии пакетов
+yum upgrade # Или обновить все пакеты, старые пакеты будут удалены
 yum -y install mc
 yum -y install net-tools.x86_64 #ifconfig
 yum -y install tcpdump #tcpdump
